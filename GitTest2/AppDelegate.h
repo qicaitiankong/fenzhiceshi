1c762467f788ded4554d2b8e00d5299a9a00f733
qicaitiankong <zihaoshuhan@163.com>
2016-11-01 18:55:48 +0800

    git 再次添加

---
+++ GitTest2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GitTest2
+//
+//  Created by 李自豪 on 16/11/1.
+//  Copyright © 2016年 李自豪. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GitTest2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GitTest2
+//
+//  Created by 李自豪 on 16/11/1.
+//  Copyright © 2016年 李自豪. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
